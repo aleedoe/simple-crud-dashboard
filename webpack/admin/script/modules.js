37e5edc606a64c20e8b7be505fab380e727c5e50
alie <mushyafaali@gmail.com>
2023-11-16 15:00:02 +0700

    menghapus file modules

--- webpack/admin/script/modules.js
+++
@@ -1,5 +0,0 @@
-import {load as load_mahasiswa, resetModalAdd as resetModalAdd_mahasiswa} from './master_mahasiswa/js/dev_module.js';
-
-$("#load-master-mahasiswa").on("click", () => {
-    load_mahasiswa();
-});
